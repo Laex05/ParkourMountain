f828363484ccbb61f79311e645aaea01dbb860d0
Alex Chandler <alex.d.chandler@gmail.com>
2026-06-12 10:13:39 -0700

    Fixed bug with no mountain and made taller

--- SpawnMountain.ts
+++ SpawnMountain.ts
@@ -28,7 +28,7 @@ async function start() {
     const colorBase = (i / 7) * 0.25;
     const colorMultiplier = (i / 7) * 0.05;
 
-    spawnCircle(2.5 + (counter * 2), 2, 2.5, i, i * 6, 1, 1, colorBase, colorMultiplier, colorBase, colorMultiplier, colorBase, colorMultiplier, true);
+    spawnCircle(2.5 + (counter * (counter / 2) * 4), 2, 2.5, i, i * 6, 1, 1, colorBase, colorMultiplier, colorBase, colorMultiplier, colorBase, colorMultiplier, true);
     i--;
   }
 }
@@ -36,7 +36,7 @@ async function start() {
 function spawnCircle(yPos: number, height: number, heightMultiplier: number, radius: number, count: number, blockRadiusStart: number, blockRadiusMultiplier: number, redColorStart: number, redColorMultiplier: number, greenColorStart: number, greenColorMultiplier: number, blueColorStart: number, blueColorMultiplier: number, hasEntrance: boolean) {
   for (let i = 0; i < count; i++) {
     const lerpPercent = i / count;
-    if (!hasEntrance || ((lerpPercent > 0.5) && (lerpPercent < 0.575))) {
+    if (!(hasEntrance && ((lerpPercent > 0.5) && (lerpPercent < 0.575)))) {
       const radianPos = Math.PI * 2 * lerpPercent;
 
       const x = Math.cos(radianPos);
