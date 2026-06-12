37c2231d5b3118798d6d66967fa0c9864000e9af
Alex Chandler <alex.d.chandler@gmail.com>
2026-06-12 09:47:16 -0700

    Added 3rd circle

--- SpawnMountain.ts
+++ SpawnMountain.ts
@@ -8,14 +8,16 @@ import { spawnPrimitive } from "./Yuu API/SpawnPrimitive";
 
 registerStart(start);
 async function start() {
-  spawnCircle(0.35, 0.7, 10, 50, 2.5, 2, 0.1, 0.05, 0.25, 0.75, 0.1, 0.05);
-
+  spawnCircle(0.35, 0.7, 10, 50, 2.5, 2, 0.1, 0.05, 0.25, 0.75, 0.1, 0.05, false);
   await Async.wait(250);
+  spawnCircle(0.75, 2, 8, 45, 1, 1, 0.05, 0.05, 0.25, 0.25, 0.05, 0.05, true);
+  await Async.wait(250);
+  spawnCircle(1.5, 2, 7, 45, 1, 1, 0.05, 0.05, 0.05, 0.25, 0.05, 0.05, true);
 
-  spawnCircle(1, 1.5, 8, 45, 0.5, 1, 0.05, 0.05, 0.25, 0.25, 0.05, 0.05);
+  // Use while loop to spawn remainder
 }
 
-function spawnCircle(yPos: number, height: number, radius: number, count: number, blockRadiusStart: number, blockRadiusMultiplier: number, redColorStart: number, redColorMultiplier: number, greenColorStart: number, greenColorMultiplier: number, blueColorStart: number, blueColorMultiplier: number) {
+function spawnCircle(yPos: number, height: number, radius: number, count: number, blockRadiusStart: number, blockRadiusMultiplier: number, redColorStart: number, redColorMultiplier: number, greenColorStart: number, greenColorMultiplier: number, blueColorStart: number, blueColorMultiplier: number, hasEntrance: boolean) {
   for (let i = 0; i < count; i++) {
     const lerpPercent = i / count;
     const radianPos = Math.PI * 2 * lerpPercent;
