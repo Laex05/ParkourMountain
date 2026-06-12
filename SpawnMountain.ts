92591c45601888709d3c578c10ebbc07b352e709
Alex Chandler <alex.d.chandler@gmail.com>
2026-06-12 09:43:05 -0700

    Slight block changes

--- SpawnMountain.ts
+++ SpawnMountain.ts
@@ -8,11 +8,11 @@ import { spawnPrimitive } from "./Yuu API/SpawnPrimitive";
 
 registerStart(start);
 async function start() {
-  spawnCircle(0.35, 0.7, 10, 50, 1.5, 2, 0.1, 0.05, 0.25, 0.75, 0.1, 0.05);
+  spawnCircle(0.35, 0.7, 11, 50, 2.5, 2, 0.1, 0.05, 0.25, 0.75, 0.1, 0.05);
 
   await Async.wait(250);
 
-  spawnCircle(1, 1.5, 9, 40, 1.5, 2, 0.05, 0.05, 0.25, 0.5, 0.05, 0.05);
+  spawnCircle(1, 1.5, 7.5, 35, 0.5, 1, 0.05, 0.05, 0.25, 0.5, 0.05, 0.05);
 }
 
 function spawnCircle(yPos: number, height: number, radius: number, count: number, blockRadiusStart: number, blockRadiusMultiplier: number, redColorStart: number, redColorMultiplier: number, greenColorStart: number, greenColorMultiplier: number, blueColorStart: number, blueColorMultiplier: number) {
