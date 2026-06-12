56799dc1b82c8e58ce931c81b6e09f6dd39cd886
Alex Chandler <alex.d.chandler@gmail.com>
2026-06-12 09:41:10 -0700

    Better function and second ring

--- SpawnMountain.ts
+++ SpawnMountain.ts
@@ -1,3 +1,4 @@
+import { Async } from "./Yuu API/Async";
 import { Color } from "./Yuu API/Basic Types/Color";
 import { Quaternion } from "./Yuu API/Basic Types/Quaternion";
 import { Vector3 } from "./Yuu API/Basic Types/Vector3";
@@ -6,11 +7,15 @@ import { spawnPrimitive } from "./Yuu API/SpawnPrimitive";
 
 
 registerStart(start);
-function start() {
-  spawnCircle(0.25, 0.5, 10, 50);
+async function start() {
+  spawnCircle(0.35, 0.7, 10, 50, 1.5, 2, 0.1, 0.05, 0.25, 0.75, 0.1, 0.05);
+
+  await Async.wait(250);
+
+  spawnCircle(1, 1.5, 9, 40, 1.5, 2, 0.05, 0.05, 0.25, 0.5, 0.05, 0.05);
 }
 
-function spawnCircle(yPos: number, height: number, radius: number, count: number) {
+function spawnCircle(yPos: number, height: number, radius: number, count: number, blockRadiusStart: number, blockRadiusMultiplier: number, redColorStart: number, redColorMultiplier: number, greenColorStart: number, greenColorMultiplier: number, blueColorStart: number, blueColorMultiplier: number) {
   for (let i = 0; i < count; i++) {
     const lerpPercent = i / count;
     const radianPos = Math.PI * 2 * lerpPercent;
@@ -18,13 +23,12 @@ function spawnCircle(yPos: number, height: number, radius: number, count: number
     const x = Math.cos(radianPos);
     const z = Math.sin(radianPos);
 
-    // add random to radius?
     const dir = new Vector3(x, 0, z);
     const pos = dir.multiply(radius);
     pos.y = yPos + (-0.5 * Math.random());
 
-    const blockRadius = 1.5 + Math.random();
+    const blockRadius = blockRadiusStart + (blockRadiusMultiplier * Math.random());
 
-    spawnPrimitive.cube(pos, new Vector3(blockRadius, height, blockRadius), Quaternion.fromEuler(new Vector3(0, Math.random() * Math.PI, 0)), new Color(0.15, 0.25 + (Math.random() * 0.75), 0.15), 1, true, 'Static', undefined);
+    spawnPrimitive.cube(pos, new Vector3(blockRadius, height, blockRadius), Quaternion.fromEuler(new Vector3(0, Math.random() * Math.PI, 0)), new Color(redColorStart + (redColorMultiplier * Math.random()), greenColorStart + (greenColorMultiplier * Math.random()), blueColorStart + (blueColorMultiplier * Math.random())), 1, true, 'Static', undefined);
   }
 }
